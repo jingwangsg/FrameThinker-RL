89ec48f9bca35edb027d1c08b5eda9e4dcbec536
hzhang <hzhange@bytedance.com>
2025-11-17 17:04:49 +0800

    update

--- experiments/1110_coldstart.fish
+++ experiments/1110_coldstart.fish
@@ -5,7 +5,7 @@ set CKPT_LORA_EP6 /mnt/amlfs-02/shared/checkpoints/jingwang/video_reason/sft/qwe
 # framethinker_coldstart_lora
 EXP=framethinker_coldstart_lora_1110 \
 ray_job_submit --skip-exists --no-wait --submission-id $EXP \
-  --runtime-env runtime_env.yaml \
+  --runtime-env ../runtime_env.yaml \
   -- bash -c "
     EXP_NAME=$EXP \
     MODEL_PATH=$CKPT_LORA \
@@ -14,7 +14,7 @@ ray_job_submit --skip-exists --no-wait --submission-id $EXP \
 
 EXP=framethinker_coldstart_ep6lora_1110 \
 ray_job_submit --skip-exists --no-wait --submission-id $EXP \
-  --runtime-env runtime_env.yaml \
+  --runtime-env ../runtime_env.yaml \
   -- bash -c "
     EXP_NAME=$EXP \
     MODEL_PATH=$CKPT_LORA_EP6 \
@@ -22,7 +22,7 @@ ray_job_submit --skip-exists --no-wait --submission-id $EXP \
 
 EXP=framethinker_coldstart_full_1110 \
 ray_job_submit --skip-exists --no-wait --submission-id $EXP \
-  --runtime-env runtime_env.yaml \
+  --runtime-env ../runtime_env.yaml \
   -- bash -c "
     EXP_NAME=$EXP \
     MODEL_PATH=$CKPT_FULL \
@@ -31,11 +31,21 @@ ray_job_submit --skip-exists --no-wait --submission-id $EXP \
 
 EXP=framethinker_coldstart_full_rpseq16k_1110 \
 ray_job_submit --skip-exists --no-wait --submission-id $EXP \
-  --runtime-env runtime_env.yaml \
+  --runtime-env ../runtime_env.yaml \
   -- bash -c "
     EXP_NAME=$EXP \
     MODEL_PATH=$CKPT_FULL \
     bash examples/agent/train_frame_thinker_vhonly.sh \
     data.max_response_length=16384
+  "
+
+EXP=framethinker_coldstart_full_ep20_1110 \
+ray_job_submit --skip-exists --no-wait --submission-id $EXP \
+  --runtime-env ../runtime_env.yaml \
+  -- bash -c "
+    EXP_NAME=$EXP \
+    MODEL_PATH=$CKPT_FULL \
+    bash examples/agent/train_frame_thinker_vhonly.sh \
+    trainer.total_epochs=20
   "
 
--- experiments/1110_new_tools.fish
+++ experiments/1110_new_tools.fish
@@ -1,7 +1,7 @@
 # framethinker_coldstart_full_toolv2_rolln16
 EXP=framethinker_coldstart_full_toolv2_rolln16_1110 \
 ray_job_submit --skip-exists --no-wait --submission-id $EXP \
-  --runtime-env runtime_env.yaml \
+  --runtime-env ../runtime_env.yaml \
   -- bash -c "
     EXP_NAME=$EXP \
     MODEL_PATH=/mnt/amlfs-02/shared/checkpoints/jingwang/video_reason/sft/qwen2_5vl_7b_full_framethinker_sft \
@@ -13,22 +13,39 @@ ray_job_submit --skip-exists --no-wait --submission-id $EXP \
 # framethinker_coldstart_full_toolv2
 EXP=framethinker_coldstart_full_toolv2_1110 \
 ray_job_submit --skip-exists --no-wait --submission-id $EXP \
-  --runtime-env runtime_env.yaml \
+  --runtime-env ../runtime_env.yaml \
   -- bash -c "
     EXP_NAME=$EXP \
     MODEL_PATH=/mnt/amlfs-02/shared/checkpoints/jingwang/video_reason/sft/qwen2_5vl_7b_full_framethinker_sft \
     bash examples/agent/train_frame_thinker_vhonly.sh \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     data.message_template=framethinker_add_zoomin
   "
 
 EXP=framethinker_coldstart_full_toolv2_rpseq16k_1110 \
 ray_job_submit --skip-exists --no-wait --submission-id $EXP \
-  --runtime-env runtime_env.yaml \
+  --runtime-env ../runtime_env.yaml \
   -- bash -c "
     EXP_NAME=$EXP \
     MODEL_PATH=/mnt/amlfs-02/shared/checkpoints/jingwang/video_reason/sft/qwen2_5vl_7b_full_framethinker_sft \
     bash examples/agent/train_frame_thinker_vhonly.sh \
     data.message_template=framethinker_add_zoomin \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
     data.max_response_length=16384
   "
+
+
+EXP=framethinker_coldstart_full_toolv2_ep20_1110 \
+ray_job_submit --skip-exists --no-wait --submission-id $EXP \
+  --runtime-env ../runtime_env.yaml \
+  -- bash -c "
+    EXP_NAME=$EXP \
+    MODEL_PATH=/mnt/amlfs-02/shared/checkpoints/jingwang/video_reason/sft/qwen2_5vl_7b_full_framethinker_sft \
+    bash examples/agent/train_frame_thinker_vhonly.sh \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    data.message_template=framethinker_add_zoomin \
+    trainer.total_epochs=20
+  "
+
+
 
